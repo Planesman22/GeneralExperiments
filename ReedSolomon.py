be04a1f1c6926e69c3da967e378a57be17a5ffd3
Planesman22 <shawnsql@gmail.com>
2023-05-15 14:27:42 -0400

    Start

--- ReedSolomon.py
+++ ReedSolomon.py
@@ -1,1 +1,9 @@
 import os
+
+
+def qr_check_qformat(Format):
+    QrGenerator = 0b10100110111
+    for I in range(4, -1, -1):
+        if Format & (1 << (I + 10)):
+            Format = Format ^ (QrGenerator << I)
+    return Format
